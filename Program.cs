406e45ac8e382acc849cd5df9662c84d2ef06136
Steven Mielke <steven.mielke@de.yusen-logistics.com>
2022-05-20 16:57:45 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmartHomeIntegrationTerminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ keywordListener.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+//documentation for the following: https://docs.microsoft.com/en-us/previous-versions/office/developer/speech-technologies/hh361683(v=office.14)
+using System.Speech.Recognition;
+//documentation for the following: https://docs.microsoft.com/en-us/previous-versions/office/developer/speech-technologies/hh361644(v=office.14)
+using System.Speech.Synthesis;
+using System.Net.NetworkInformation;
+
+namespace SmartHomeIntegrationTerminal
+{
+    class keywordListener
+    {
+
+    }
+}
