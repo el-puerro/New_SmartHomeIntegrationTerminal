ceafbcb719ef83da35d25f8e54fb25627d0e184a
Lucy <mielkesteven@icloud.com>
2022-06-26 20:26:03 +0200

    minor changes

--- activeListener.cs
+++ activeListener.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace SmartHomeIntegrationTerminal
 {
     class activeListener
     {
+        private string[] availableDevices;
+        private string[] deviceAddresses;
+
+        public String setDevices(string[] deviceNames, string[] deviceAddresses)
+        {
+            // Black magic IP-Validation via Regular Expression begins
+            Regex regex = new Regex(@"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b");
+            
+            private string[] invalidDevices;
+            private string[] invalidAddresses;
+            for(int i = 0; i < deviceAddresses.Length)
+            {
+                if(regex.Matches(deviceAddresses[i])) // Black magic ends here
+                {
+                    this.availableDevices[i] = deviceNames[i];
+                    this.deviceAddresses[i] = deviceAddresses[i];
+                }
+                else
+                {
+                    invalidDevices[i] = deviceNames[i];
+                }
+            }
+            
+            invalidDevices.Length == 0 ? return "OK" : return String.Concat(invalidDevices);
+        }
+
+        public string[] getDeviceNames() => this.availableDevices;
+        public string[] getDeviceAddresses() => this.deviceAddresses;
     }
 }
--- keywordListener.cs
+++ keywordListener.cs
@@ -9,11 +9,19 @@ using System.Speech.Recognition;
 //documentation for the following: https://docs.microsoft.com/en-us/previous-versions/office/developer/speech-technologies/hh361644(v=office.14)
 using System.Speech.Synthesis;
 using System.Net.NetworkInformation;
+using System.Text.RegularExpressions;
 
 namespace SmartHomeIntegrationTerminal
 {
     class keywordListener
     {
+        private boolean keywordFound;
 
+
+        public keywordListener(string[] devices, string[] IPs)
+        {
+            setDevices(devices, IPs);
+            SpeechRecognizer sr = new SpeechRecognizer();
+        }
     }
 }
